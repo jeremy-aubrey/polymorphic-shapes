09eb9614a88e6a71ff20a7af6ed322aa0b92d09c
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-10 12:51:02 -0600

    implemented toString method

--- src/project2/Tetrahedron.java
+++ src/project2/Tetrahedron.java
@@ -14,10 +14,16 @@ public class Tetrahedron extends ThreeDimensionalShape {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
 	@Override
 	public double getVolume() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%-10s %s%n%-9s %s%n", "Shape:", shapeName, "Dimension:", dimension);
 	}
 	
 }
