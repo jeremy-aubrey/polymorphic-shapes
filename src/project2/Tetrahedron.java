d240cba5cf4195095bec9a99d0e3555ba5c27ff3
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-10 11:35:35 -0600

    added private attribute: edge; implemented constructor

--- src/project2/Tetrahedron.java
+++ src/project2/Tetrahedron.java
@@ -2,15 +2,22 @@ package project2;
 
 public class Tetrahedron extends ThreeDimensionalShape {
 
-	public Tetrahedron(String shapeName) {
-		super(shapeName);
-		// TODO Auto-generated constructor stub
+	private double edge;
+	
+	public Tetrahedron(double edge) {
+		super("Tetrahedron");
+		this.edge = edge;
 	}
 
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	@Override
 	public double getVolume() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
 }
