3de3537489a37c3c3b015e547242632f1e5ec7a1
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-17 13:32:37 -0600

    implemented getArea(); added area to toString()

--- src/project2/Tetrahedron.java
+++ src/project2/Tetrahedron.java
@@ -19,8 +19,7 @@ public class Tetrahedron extends ThreeDimensionalShape {
 
 	@Override
 	public double getArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.sqrt(3) * (edge * edge);
 	}
 
 	@Override
@@ -31,7 +30,10 @@ public class Tetrahedron extends ThreeDimensionalShape {
 	
 	@Override
 	public String toString() {
-		return String.format("%-10s %s%n%-9s %s%n", "Shape:", getShapeName(), "Dimension:", getDimension());
+		return String.format("%-10s %s%n%-9s %s%n%-10s %s%n", 
+				"Shape:", getShapeName(), 
+				"Dimension:", getDimension(),
+				"Area:", String.format("%.1f", getArea()));
 	}
 	
 }
