df17cab489e37357b27bc2d7f4919dae39856029
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-17 15:17:57 -0600

    added module and method comments

--- src/project2/Sphere.java
+++ src/project2/Sphere.java
@@ -1,39 +1,132 @@
+//******************************************************************************
+//
+//  Developer:     Jeremy Aubrey
+//
+//  Project #:     Project 2
+//
+//  File Name:     Sphere.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      2/20/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Description:   A concrete class for a sphere that extends ThreeDimensionalShape 
+//                 which extends Shape.
+//
+//******************************************************************************
+
 package project2;
 
 public class Sphere extends ThreeDimensionalShape {
 
 	private double radius;
 	
+	// constructor
 	public Sphere(double radius) {
+		
 		super("Sphere");
 		this.radius = radius;
-	}
+		
+	}// end constructor
 
+   //***************************************************************
+   //
+   //  Method:       getShapeName (Non Static)
+   // 
+   //  Description:  Returns the name of the shape by calling super
+   //                class method.
+   //
+   //  Parameters:   None
+   //
+   //  Returns:      String
+   //
+   //***************************************************************
 	public String getShapeName() {
+		
 		return super.getShapeName();
-	}
+		
+	}// end getShapeName method
 	
+   //***************************************************************
+   //
+   //  Method:       getDimension (Non Static)
+   // 
+   //  Description:  Returns the dimension of the shape by calling super
+   //                class method.
+   //
+   //  Parameters:   None
+   //
+   //  Returns:      String
+   //
+   //***************************************************************
 	public String getDimension() {
+		
 		return super.getDimension();
-	}
+		
+	}// end getDimension method
 	
+    //***************************************************************
+    //
+    //  Method:       getArea (Non Static)
+    // 
+    //  Description:  Implements the inherited, abstract getArea method 
+    //                of Shape. Calculates and returns the area of a sphere.
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      double 
+    //
+    //***************************************************************
 	@Override
 	public double getArea() {
+		
 		return 4.0 * PI * (radius * radius);
-	}
+		
+	}// end getArea method
 	
+    //***************************************************************
+    //
+    //  Method:       getVolume (Non Static)
+    // 
+    //  Description:  Implements the inherited, abstract getVolume method 
+    //                of ThreeDimensionalShape. Calculates and returns the 
+    //                volume of a sphere.
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      double 
+    //
+    //***************************************************************
 	@Override
 	public double getVolume() {
+		
 		return (4.0/3.0) * PI * (radius * radius * radius);
-	}
+		
+	}// end getVolume method
 	
+    //***************************************************************
+    //
+    //  Method:       toString (Non Static)
+    // 
+    //  Description:  Returns a formatted shape description including 
+    //                its name, dimension, area, and volume.
+    //	
+    //  Parameters:   None
+    //
+    //  Returns:      String 
+    //
+    //***************************************************************
 	@Override
 	public String toString() {
+		
 		return String.format("%-10s %s%n%-9s %s%n%-10s %s%n%-10s %s%n", 
 				"Shape:", getShapeName(), 
 				"Dimension:", getDimension(),
 				"Area:", String.format("%.1f", getArea()),
 				"Volume:", String.format("%.1f", getVolume()));
-	}
+		
+	}// end toString method
 
-}
+}// end Sphere class
