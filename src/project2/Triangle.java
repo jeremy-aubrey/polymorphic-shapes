172936a89b2d5066d29a5a2ec900af1d56ea11c7
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-17 14:43:45 -0600

    added module and method comments

--- src/project2/Triangle.java
+++ src/project2/Triangle.java
@@ -1,3 +1,22 @@
+//******************************************************************************
+//
+//  Developer:     Jeremy Aubrey
+//
+//  Project #:     Project 2
+//
+//  File Name:     Triangle.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      2/20/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Description:   A concrete class for a triangle that extends TwoDimensionalShape 
+//                 which extends Shape.
+//
+//******************************************************************************
+
 package project2;
 
 public class Triangle extends TwoDimensionalShape {
@@ -5,31 +24,88 @@ public class Triangle extends TwoDimensionalShape {
 	private double base;
 	private double height;
 	
+	// constructor
 	public Triangle(double base, double height) {
+		
 		super("Triangle");
 		this.base = base;
 		this.height = height;
-	}
+		
+	}// end constructor
 	
+   //***************************************************************
+   //
+   //  Method:       getShapeName (Non Static)
+   // 
+   //  Description:  Returns the name of the shape.
+   //
+   //  Parameters:   None
+   //
+   //  Returns:      String
+   //
+   //***************************************************************
 	public String getShapeName() {
+		
 		return super.getShapeName();
-	}
+		
+	}// end getShapeName method
 	
+   //***************************************************************
+   //
+   //  Method:       getDimension (Non Static)
+   // 
+   //  Description:  Returns the dimension of the shape.
+   //
+   //  Parameters:   None
+   //
+   //  Returns:      String
+   //
+   //***************************************************************
 	public String getDimension() {
+		
 		return super.getDimension();
-	}
+		
+	}// end getDimension method
 
+    //***************************************************************
+    //
+    //  Method:       getArea (Non Static)
+    // 
+    //  Description:  Implements the inherited, abstract getArea method 
+    //                of Shape. Calculates and returns the area of a triangle.
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      double 
+    //
+    //***************************************************************
 	@Override
 	public double getArea() {
+		
 		return (base * height) / 2.0;
-	}
+		
+	}// end getArea method
 
+    //***************************************************************
+    //
+    //  Method:       toString (Non Static)
+    // 
+    //  Description:  Returns a formatted shape description including 
+    //                its name, dimension, and area.
+    //	
+    //  Parameters:   None
+    //
+    //  Returns:      String 
+    //
+    //***************************************************************
 	@Override
 	public String toString() {
+		
 		return String.format("%-10s %s%n%-9s %s%n%-10s %s%n", 
 				"Shape:", getShapeName(), 
 				"Dimension:", getDimension(),
 				"Area:", String.format("%.1f", getArea()));
-	}
+		
+	}// end toString method
 	
-}
+}// end Triangle class
